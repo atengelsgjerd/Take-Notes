2e94f981498a5649a44bcd3e665e9aa585970230
atengelsgjerd <atengelsgjerd@gmail.com>
2024-05-17 15:06:55 -0500

    fixed bugs with linking stylesheet correctly

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+const db = require('./db/db.json');
+
 const PORT = 3001;
 
 const app = express();
@@ -11,13 +13,29 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+//html routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, + '/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
+//api routes
+app.get('/api/notes', (req, res) => {
+  res.json(db);
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = {
+    id: req.body.noteId,
+    title: req.body.title,
+    text: req.body.text
+  }
+  db.push(newNote);
+})
+
 
 console.log(__dirname);
 
