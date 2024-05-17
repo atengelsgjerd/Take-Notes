92165b57a43713c6540185fdf4242014870a7435
atengelsgjerd <atengelsgjerd@gmail.com>
2024-05-17 14:01:32 -0500

    moved folders/files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const PORT = 3001;
 
@@ -12,6 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 console.log(__dirname);
