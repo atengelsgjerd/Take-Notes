5acaaef84b010143fa116cba53bc875ca2b36ac1
atengelsgjerd <atengelsgjerd@gmail.com>
2024-05-18 12:04:51 -0500

    Wrote code for data persistence

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
+// Purpose: This file is the entry point for the application. It will start the server and listen for incoming requests.
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+
+const uuId = require('./helpers/randomId.js');
 
 const db = require('./db/db.json');
 
 const PORT = 3001;
 
+// Create an instance of the express app.
+
 const app = express();
 
-app.use(express.static('public'));
+
+// Middleware for parsing JSON and urlencoded form data
+
+app.use(express.static('public/'));
 app.use(express.json());
+
+
+// Parse incoming string or array data
 
 app.use(express.urlencoded({ extended: true }));
 
 //html routes
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, + '/public/index.html'));
 });
+
+//notes route
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
@@ -27,13 +42,46 @@ app.get('/api/notes', (req, res) => {
   res.json(db);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to read file' });
+    }
+    for (let i = 0; i < db.length; i++) {
+      if (db[i].id === noteId) {
+        return res.json(db[i]);
+      }
+    }
+    res.json({ error: 'Note not found' });
+  });
+});
+
+
+
 app.post('/api/notes', (req, res) => {
   const newNote = {
-    id: req.body.noteId,
+    id: uuId(),
     title: req.body.title,
     text: req.body.text
   }
   db.push(newNote);
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+    fs.writeFile('./db/db.json', JSON.stringify(notes, "", 4), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.json(newNote);
+    });
+  });
 })
 
 
